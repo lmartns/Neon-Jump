98271d8e248e85568360af2bb7ea83d13e1fcbcc
Leonardo Martins <94061150+leomartinss@users.noreply.github.com>
2022-08-01 21:13:57 -0300

    Update

---
+++ Assets/Scripts/Player/Controller.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private float moveInput;
+    private float speed;
+ 
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    void FixedUpdate()
+    {
+
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+    }
+}
